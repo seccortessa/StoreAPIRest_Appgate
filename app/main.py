fcd0f2366ab76e9b7e0c9155cecf0a78c78d400f
seccortessa <seccortessa@unal.edu.co>
2024-09-11 15:17:32 -0500

    primary version of Rest implemented

--- app/main.py
+++ app/main.py
@@ -30,4 +30,18 @@ async def get_price(application_date: datetime, product_id: int, brand_id: int, 
     ).order_by(models.Price.priority.desc()).first()
     # takes the first result, since it is ordered by priority
     
-    return prices
+    
+    # organize and present the desired results
+    if prices:
+    
+        return {
+            "product_id": prices.product_id,
+            "brand_id": prices.brand_id,
+            "price_list": prices.price_list,
+            "start_date": prices.start_date,
+            "end_date": prices.end_date,
+            "price_type": prices.price,
+            "currency": prices.curr
+        }
+    else:
+        return {"error": "No price found for the specified parameters"}
