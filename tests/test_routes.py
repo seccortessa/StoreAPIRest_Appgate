98e2833215616e7b1cf931a57bd7df2d924d50d6
seccortessa <seccortessa@unal.edu.co>
2024-09-12 02:53:35 -0500

    dockerfile crated

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -6,12 +6,14 @@ import pytest
 
 client = TestClient(app)
 
+# main test fpr functionality
 def test_root():
     response = client.get('/')
     assert response.status_code == status.HTTP_200_OK
     assert response.json()['status'] == "API is running!"
 
 
+# tests for invalid/out of range parameters
 def test_invalid_product_id():
     response = client.get('/price/', params = {"product_id": 9999, "brand_id": 1, "application_date": "2020-06-15T10:00:00"})
     assert response.status_code == status.HTTP_404_NOT_FOUND
@@ -27,12 +29,8 @@ def test_invalid_date():
     assert response.status_code == status.HTTP_404_NOT_FOUND
     assert response.json()["detail"] == "Entered date out of range"
 
-# specific tests
-def test_1():
-    response = client.get('/price/', params = {"product_id": 35455, "brand_id": 1, "application_date": "2020-06-14T10:00:00"})
-    assert response.status_code == status.HTTP_200_OK
-    assert response.json()
-    
+
+# set of params and expected responses parametrized for tests
 @pytest.mark.parametrize(
     "application_date, product_id, brand_id, expected_status_code, expected_response",
     [
@@ -42,14 +40,11 @@ def test_1():
         ("2020-06-15T21:00:00", 35455, 1, status.HTTP_200_OK, {"product_id": 35455,"brand_id": 1, "price_list": 4, "start_date": "2020-06-15T16:00:00", "end_date": "2020-12-31T23:59:59", "price": 38.95})
     ],
 )
-    
+
+# perform all the four tests woth the given parameters
 def test_specific(application_date, product_id, brand_id, expected_status_code, expected_response):
-
     response = client.get('/price/', params = {"product_id": product_id, "brand_id": brand_id, "application_date": application_date})
-    
     assert response.status_code == expected_status_code
-    
     assert response.status_code == status.HTTP_200_OK
-
     if expected_status_code == status.HTTP_200_OK:
         assert response.json() == expected_response
